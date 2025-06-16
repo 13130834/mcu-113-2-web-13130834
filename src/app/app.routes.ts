c4af5df0fe14cf50c36f870a20c14142179ec242
k <kelly940907@gmail.com>
2025-06-16 14:54:12 +0800

    feat(router): 切換產品清單與登入頁面實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ArrowFunctionExpr } from '@angular/compiler';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'products', component: ProductPageComponent},
+  { path: 'login', component: LoginPageComponent },
+];
